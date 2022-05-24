1a89fef2d2f36a48385a714869c4e065e769e138
Pedro Machado <pedro.machado@valtech.com>
2022-05-24 16:04:22 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import Todo from "./components/Pages/Todo";
+
+function App() {
+  return <Todo />;
+}
+
+export default App;
---
+++ src/components/Pages/Todo.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Todo() {
+  return <div>Teste</div>;
+}
+
+export default Todo;
