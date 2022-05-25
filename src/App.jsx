17c8a7c79690d41ba579f35b4c0358aefba6476b
Pedro Machado <pedro.machado@valtech.com>
2022-05-25 14:59:08 -0300

    variables and switch theme toggle set

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,14 @@
+import { useState } from "react";
 import Todo from "./components/Pages/Todo";
+import styles from "./App.module.scss";
 
 function App() {
+  const [theme, setTheme] = useState("dark");
+
+  const switchTheme = () => {
+    setTheme(theme === "dark" ? "light" : "dark");
+  };
+
   return <Todo />;
 }
 
