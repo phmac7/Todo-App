e7577cafb580e37769e1b6a880146d43ce6fc798
Pedro Machado <phvieiras@hotmail.com>
2022-05-30 19:09:50 -0300

    Drag and Drop implemented

--- src/components/UI/molecules/TaskItem/TaskItem.jsx
+++ src/components/UI/molecules/TaskItem/TaskItem.jsx
@@ -4,8 +4,9 @@ import Checkbox from '../../atoms/Checkbox/Checkbox'
 import Cross from '../../atoms/Svg/Cross'
 import styles from './TaskItem.module.scss'
 import { TaskListContext } from '/src/context/TaskListContext'
+import { useDraggable, Draggable, moveItems } from 'react-tiny-dnd'
 
-function TaskItem({ task, id }) {
+function TaskItem({ task, id, context, index }) {
     const { taskList, toggleCompleted, toggleNotCompleted, removeTaskHandler, filter } = useContext(TaskListContext)
     const [isCompleted, setIsCompleted] = useState(taskList.find((task) => task.id === id).completed)
     const getToggle = () => {
@@ -25,21 +26,31 @@ function TaskItem({ task, id }) {
             return true
         }
     }
+
+    const {
+        listeners, // Handler listeners can be passed to Draggable component as well
+        isDragging,
+    } = useDraggable(context, index);
+
     return (
-        <li
-            className={`${styles['task__list--item']} ${checkingFilter() ? styles.hide : ''}`}
-            key={task.id}
-        >
-            <Card>
-                <div className={styles['task__list--item-flex']}>
-                    <Checkbox isCompleted={isCompleted} onClick={getToggle} />
-                    <span className={`${styles[`task__list--text`]} ${isCompleted ? styles.checked : ''} `}>{task}</span>
-                    <button type='button' className={styles['cross-button']} onClick={() => removeTaskHandler(id)}>
-                        <Cross />
-                    </button>
-                </div>
-            </Card>
-        </li>
+        <Draggable context={context} key={id} index={index}>
+            <li
+                className={`${styles['task__list--item']} ${checkingFilter() ? styles.hide : ''}`}
+                key={id}
+                item={task}
+                {...listeners}
+            >
+                <Card>
+                    <div className={styles['task__list--item-flex']}>
+                        <Checkbox isCompleted={isCompleted} onClick={getToggle} />
+                        <span className={`${styles[`task__list--text`]} ${isCompleted ? styles.checked : ''} `}>{task}</span>
+                        <button type='button' className={styles['cross-button']} onClick={() => removeTaskHandler(id)}>
+                            <Cross />
+                        </button>
+                    </div>
+                </Card>
+            </li>
+        </Draggable>
 
     )
 }
--- src/components/UI/organisms/TaskList/TaskList.jsx
+++ src/components/UI/organisms/TaskList/TaskList.jsx
@@ -1,12 +1,21 @@
 import React, { useContext, useId } from 'react'
-import Card from '../../atoms/Card/Card'
 import TaskFilters from '../../molecules/TaskFilters/TaskFilters'
 import TaskItem from '../../molecules/TaskItem/TaskItem'
 import styles from './TaskList.module.scss'
 import { TaskListContext } from '/src/context/TaskListContext'
+import { useDraggableContext, moveItems } from 'react-tiny-dnd'
 
 function TaskList() {
-    const { taskList } = useContext(TaskListContext)
+    const { taskList, setTaskList } = useContext(TaskListContext)
+
+    const onDrop = (dragIndex, overIndex) => {
+        const nextItems = moveItems(taskList, dragIndex, overIndex);
+        setTaskList(nextItems);
+    };
+
+    const context = useDraggableContext({
+        onDrop,
+    });
 
     return (
         <>
@@ -14,9 +23,9 @@ function TaskList() {
 
                 {typeof (taskList) === 'undefined' ? null :
 
-                    taskList.map((task) => {
+                    taskList.map((task, index) => {
                         return (
-                            <TaskItem task={task.task} id={task.id} key={task.id} />
+                            <TaskItem context={context} task={task.task} id={task.id} key={task.id} index={index} />
                         )
                     })}
 
